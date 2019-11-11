b2f0dd89f09c9e23b8d7f6a25f9abbe3d2067b17
jncro-iscteiul <jncro@iscte-iul.pt>
2019-11-11 16:43:35 +0000

    Git a funcionar

--- src/Principal.java
+++ src/Principal.java
@@ -3,6 +3,6 @@ public class Principal {
 
 	
 	public static void main(String []args) {
-		System.out.println("git a funcionar - Pedro Santos");
+		System.out.println("git a funcionar - Pedro Santos" + "\n" + "-Ramalho");
 	}
 }
