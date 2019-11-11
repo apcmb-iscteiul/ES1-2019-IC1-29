aef2dee297054131e4fd045c7edfb243a699e6e2
arcro1-iscteiul <arcro1@iscte-iul.pt>
2019-11-11 17:42:38 +0000

    Xonito

--- src/Principal.java
+++ src/Principal.java
@@ -3,6 +3,6 @@ public class Principal {
 
 	
 	public static void main(String []args) {
-		System.out.println("git a funcionar - Pedro Santos" + "\n" + "-Ramalho" + "\n" + "Atadjo");
+		System.out.println("git a funcionar - Pedro Santos" + "\n" + "-Ramalho" + "\n" + "Atadjo" + "Xonito");
 	}
 }
